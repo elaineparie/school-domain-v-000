118b32b3d5ca9d48316db141ad676bc26efbc521
Elaine Anderson <eparie@bu.edu>
2018-05-30 16:29:52 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def roster=(roster)
+    @roster = roster
+  end
+
+  def roster
+    @roster = {}
+end
+
+def add_student=(student, grade)
+  @student = student
+  @grade = grade
+end
+
+def add_student(student, grade)
+  @roster[@grade] = []
+  @roster[@grade] << @student
+
+end
+
+
+end
